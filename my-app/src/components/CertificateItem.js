5124c0b46907f37b5d8f74889954e1f3870e5b7c
unknown <guilhermetaliberti@gmail.com>
2024-02-05 13:45:02 -0300

    layout update

--- my-app/src/components/CertificateItem.js
+++ my-app/src/components/CertificateItem.js
@@ -1,12 +1,12 @@
 import "../Styles/CertificateItem.css"
 
-export default function CertificateItem ({title, src, alt, url}) {
+export default function CertificateItem ({title, school, src, alt, url}) {
     return(
         <div className="certificateitem">
             <h1 className="certificatetitle">
-                {title}
+                {`${title} - ${school}`}
             </h1>
-            <a className="certificatelink" href={src} target="_blank" rel="noopener noreferrer">
+            <a className="certificatelink" href={url} target="_blank" rel="noopener noreferrer">
                 <img className="certificateimage" src={src} alt={alt}></img>
             </a>
         </div>
--- my-app/src/components/CertificateSection.js
+++ my-app/src/components/CertificateSection.js
@@ -6,10 +6,17 @@ import "slick-carousel/slick/slick-theme.css"
 
 export default function CertificateSection () {
     const certificates = [
-        {title:"NodeJS Avançado", src:"nodejsavançado.jpg", alt:"nodejs avançado", url:"link do certificado"},
-        {title:"JavaScript Avançado", src:"javascriptavançado.jpg", alt:"JavaScript avançado", url:"link do certificado"},
-        {title:"JavaScript Avançado", src:"javascriptavançado.jpg", alt:"JavaScript avançado", url:"link do certificado"},
-        {title:"JavaScript Avançado", src:"javascriptavançado.jpg", alt:"JavaScript avançado", url:"link do certificado"},
+        {title:"NodeJS Avançado", school:"Dev Samurai", src:"nodejsavançado.jpg", alt:"nodejs avançado", url:"link do certificado"},
+        {title:"JavaScript Avançado", school:"Dev Samurai", src:"javascriptavançado.jpg", alt:"JavaScript avançado", url:"link do certificado"},
+        {title:"Banco de Dados Avançado", school:"Dev Samurai", src:"bancodedadosavançado.jpg", alt:"Banco de Dados avançado", url:"link do certificado"},
+        {title:"HTML e CSS Avançado", school:"Dev Samurai", src:"htmlecssavançado.jpg", alt:"HTML e CSS avançado", url:"link do certificado"},
+        {title:"NodeJS Básico", school:"Dev Samurai", src:"nodejsbásico.jpg", alt:"nodejs Básico", url:"link do certificado"},
+        {title:"JavaScript Básico", school:"Dev Samurai", src:"javascriptbásico.jpg", alt:"JavaScript Básico", url:"link do certificado"},
+        {title:"Banco de Dados Básico", school:"Dev Samurai", src:"bancodedadosbásico.jpg", alt:"Banco de Dados Básico", url:"link do certificado"},
+        {title:"HTML e CSS Básico", school:"Dev Samurai", src:"htmlecssbásico.jpg", alt:"HTML e CSS Básico", url:"link do certificado"},
+        {title:"ReactJS", school:"Danki Code", src:"reactjs.jpg", alt:"ReactJS", url:"link do certificado"},
+        {title:"JAVA Fundamentos", school:"FIAP", src:"javafundamentos.jpg", alt:"JAVA Fundamentos", url:"link do certificado"},
+        {title:"CyberSecurity", school:"FIAP", src:"cybersecurity.jpg", alt:"CyberSecurity", url:"link do certificado"},
     ]
 
     const settings = {
@@ -18,6 +25,7 @@ export default function CertificateSection () {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        arrows: false,
     };
 
     return (
@@ -29,7 +37,7 @@ export default function CertificateSection () {
             <div className="certificatespace">
                 <Slider className="slider" {...settings}>
                     {certificates.map((certificate, index) => (
-                        <CertificateItem key={index} title={certificate.title} src={certificate.src} alt={certificate.alt} url={certificate.url}/>
+                        <CertificateItem key={index} title={certificate.title} school={certificate.school} src={certificate.src} alt={certificate.alt} url={certificate.url}/>
                     ))}
                 </Slider>
             </div>
